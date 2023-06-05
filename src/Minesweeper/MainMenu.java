7e7aa2e47388da87b99b40f925e8083dc616c451
bourrasque <stampixel.t@gmail.com>
2023-06-05 10:04:07 -0400

    Commit on 'main'

--- src/Minesweeper/MainMenu.java
+++ src/Minesweeper/MainMenu.java
@@ -1,4 +1,12 @@
 package Minesweeper;
 
 public class MainMenu extends Window {
+    MainMenu () {
+        super();
+    }
+
+    public static void main(String[] args) {
+        new MainMenu();
+    }
 }
+
--- src/Minesweeper/Window.java
+++ src/Minesweeper/Window.java
@@ -1,4 +1,13 @@
 package Minesweeper;
 
-public abstract class Window {
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class Window extends JFrame {
+    Window() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false); // Prevent frame from being resized
+        this.setSize(500, 500); // Sets width:height
+        this.setVisible(true); // Make frame visible
+    }
 }
