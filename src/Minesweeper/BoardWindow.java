3fb4845b1b3c3248dc6c89594bf16c1ee0453b36
bourrasque <stampixel.t@gmail.com>
2023-06-16 15:21:49 -0400

    gameBoard

--- src/Minesweeper/BoardWindow.java
+++ src/Minesweeper/BoardWindow.java
@@ -14,8 +14,8 @@ public class BoardWindow extends JFrame implements ActionListener {
     int mineHeight;
     int mineWidth;
     boolean firstClick;
-    int row;
-    int col;
+    int ROW;
+    int COL;
     int mineNum;
     JPanel header;
     JPanel mineField;
@@ -32,32 +32,43 @@ public class BoardWindow extends JFrame implements ActionListener {
     ImageIcon flag;
     ImageIcon emptySquare;
     ImageIcon coveredSquare;
+    int[][] navigation;
+    ImageIcon[] mineImg;
 
 
     BoardWindow(int mineRow, int mineCol, int mineNum) {
-        one = new ImageIcon("1.png");
-        two = new ImageIcon("2.png");
-        three = new ImageIcon("3.png");
-        four = new ImageIcon("4.png");
-        five = new ImageIcon("5.png");
-        six = new ImageIcon("6.png");
-        seven = new ImageIcon("7.png");
-        eight = new ImageIcon("8.png");
+
+        navigation = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}}; // up, right, down, left (clockwise check 8 adjacent squares)
+
+        mineImg = new ImageIcon[9];
+
+        for (int i = 0; i < mineImg.length; i++) {
+            mineImg[i] = new ImageIcon(i + ".png");
+        }
+
+//        one = new ImageIcon("1.png");
+//        two = new ImageIcon("2.png");
+//        three = new ImageIcon("3.png");
+//        four = new ImageIcon("4.png");
+//        five = new ImageIcon("5.png");
+//        six = new ImageIcon("6.png");
+//        seven = new ImageIcon("7.png");
+//        eight = new ImageIcon("8.png");
         mine = new ImageIcon("Mine.png");
         flag = new ImageIcon("Flag.png");
-        emptySquare = new ImageIcon("EmptySquare.png");
+//        emptySquare = new ImageIcon("0.png");
         coveredSquare = new ImageIcon("ClickableSquare.png");
 
         mineHeight = 48;
         mineWidth = 48;
 
         firstClick = true;
-        row = mineRow;
-        col = mineCol;
+        ROW = mineRow;
+        COL = mineCol;
 
-        mineFieldArray = new JButton[row][col];
-        mineRepresentationArray = new int[row][col];
-        mineAdjacentMatrix = new int[row][col];
+        mineFieldArray = new JButton[ROW][COL];
+        mineRepresentationArray = new int[ROW][COL];
+        mineAdjacentMatrix = new int[ROW][COL];
 
         this.mineNum = mineNum;
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -76,7 +87,7 @@ public class BoardWindow extends JFrame implements ActionListener {
         header.setPreferredSize(new Dimension(WIDTH, 80));
 
         mineField.setBackground(Color.darkGray);
-        mineField.setLayout(new GridLayout(row, col, 3, 3));
+        mineField.setLayout(new GridLayout(ROW, COL, 3, 3));
 
         footer.setBackground(Color.gray);
         footer.setPreferredSize(new Dimension(WIDTH, 80));
@@ -85,14 +96,14 @@ public class BoardWindow extends JFrame implements ActionListener {
         this.add(mineField, BorderLayout.CENTER);
         this.add(footer, BorderLayout.SOUTH);
 
-        generateMineField(row, col);
+        generateMineField(ROW, COL);
 
         this.setVisible(true);
         UpdateBoard();
     }
 
     public static void main(String[] args) {
-        new BoardWindow(9, 9, 50);
+        new BoardWindow(8, 8, 10);
     }
 
     /**
@@ -101,7 +112,7 @@ public class BoardWindow extends JFrame implements ActionListener {
      * @param mineLocations
      */
     public void placeMines(int[][] mineLocations) {
-        for (int i = 1; i < mineLocations.length; i++) {
+        for (int i = 9; i < mineLocations.length; i++) {
             mineRepresentationArray[mineLocations[i][0]][mineLocations[i][1]] = 2; // Set the location to a mine
         }
     }
@@ -112,11 +123,66 @@ public class BoardWindow extends JFrame implements ActionListener {
                 if (mineRepresentationArray[i][j] == 0) {
                     mineFieldArray[i][j].setIcon(coveredSquare);
                 } else if (mineRepresentationArray[i][j] == 1) {
-                    mineFieldArray[i][j].setIcon(emptySquare);
+                    int mineCount = 0;
+                    for (int k = 0; k < navigation.length; k++) {
+                        try {
+                            if (mineRepresentationArray[i+navigation[k][0]][j+navigation[k][1]] == 2) {
+                                mineCount+=1;
+                            }
+                        } catch (Exception e) {
+//                            System.out.println("outside of square, pass");
+                        }
+                    }
+
+                    mineFieldArray[i][j].setIcon(mineImg[mineCount]);
                 }
             }
         }
+    }
 
+    public boolean checkAdjacentMine(int row, int col) {
+        boolean adjacent = false;
+        for (int i = 0; i < navigation.length; i++) {
+            try {
+                if (mineRepresentationArray[row+navigation[i][0]][col+navigation[i][1]] == 2) {
+                    adjacent = true;
+                }
+            } catch (Exception e) {
+
+            }
+        }
+        return adjacent;
+    }
+
+    public void floodFill(int row, int col) {
+        int fillRow;
+        int fillCol;
+        for (int i = 0; i < mineRepresentationArray.length; i++) {
+            for (int j = 0; j < mineRepresentationArray[0].length; j++) {
+                System.out.print(mineRepresentationArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < navigation.length; i++) {
+            try {
+
+                fillRow = row + navigation[i][0];
+                fillCol = col + navigation[i][1];
+                if (mineRepresentationArray[fillRow][fillCol] == 0) {
+                    if (checkAdjacentMine(fillRow, fillCol)) {
+                        mineRepresentationArray[fillRow][fillCol] = 1;
+                        continue;
+                    }
+                    mineRepresentationArray[fillRow][fillCol] = 1;
+
+                    floodFill(fillRow, fillCol);
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
     }
 
     public void showMine() {
@@ -153,15 +219,21 @@ public class BoardWindow extends JFrame implements ActionListener {
     public void generateBoard(int clickRow, int clickCol) {
         // Min + (int)(Math.random() * ((Max - Min) + 1))
         int row, col;
-        int mineCount = 1; // Start at one since the first mine in mineLocation represent the first square clicked
+        int mineCount = 9; // Start at one since the first mine in mineLocation represent the first square clicked
         row = mineRepresentationArray.length;
         col = mineRepresentationArray[0].length;
 
-        int[][] mineLocations = new int[mineNum + 1][2]; // Example with 2 mines: {{ROW, COL}, {ROW, COL}}
+        int[][] mineLocations = new int[mineNum + 9][2]; // Example with 2 mines: {{ROW, COL}, {ROW, COL}}
         mineLocations[0][0] = clickRow;
         mineLocations[0][1] = clickCol;
 
-        while (mineCount != mineNum + 1) { // +1 since we added 1 to mineCount
+        for (int i = 1; i < navigation.length; i++) { // Generates safe zone around mine
+            if (clickRow+navigation[i][0] < ROW && clickCol+navigation[i][1] < COL)
+            mineLocations[i][0] = clickRow+navigation[i][0];
+            mineLocations[i][1] = clickCol+navigation[i][1];
+        }
+
+        while (mineCount != mineNum + 9) { // +1 since we added 1 to mineCount
             int mineRow, mineCol;
             boolean mineExist;
 
@@ -198,18 +270,21 @@ public class BoardWindow extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
 
-        System.out.println(row);
+        System.out.println(ROW);
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
                 if (e.getSource() == mineFieldArray[i][j]) {
                     if (firstClick) {
                         firstClick = false;
                         generateBoard(i, j);
-
+                        mineRepresentationArray[i][j] = 1; // Uncover the square
+                        floodFill(i, j);
                     } else if (mineRepresentationArray[i][j] == 0) { // If user clicked on a covered square
                         mineRepresentationArray[i][j] = 1; // Uncover the square
-                        // run flood fill algorithm
+                        if (!checkAdjacentMine(i, j)) {
+                            floodFill(i, j);
+                        }
                     } else if (mineRepresentationArray[i][j] == 2) { // If user clicked on a mine
                         showMine();
                     }
@@ -217,7 +292,5 @@ public class BoardWindow extends JFrame implements ActionListener {
                 }
             }
         }
-
-
     }
 }
